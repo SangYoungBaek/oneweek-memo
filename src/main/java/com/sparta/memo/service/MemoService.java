48c6606a5f66a02e1640ff1824270613f22f0a7a
SangYoungBaek <bsy2274@naver.com>
2023-08-30 12:08:06 +0900

    SpringBoopt Data JPA란 무엇일까?

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -7,6 +7,7 @@ import com.sparta.memo.repository.MemoRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -33,36 +34,36 @@ public class MemoService {
 
     public List<MemoResponseDto> getMemos() {
         // DB 조회
-        return memoRepository.findAll();
+        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
     }
 
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
         // 메모 업데이트
-
+        // 에러가 나는 이유는 Optional 이라서 에러가 발생한다 . 널체크를 해줘야한다 꼭
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            // memo 내용 수정
-            memoRepository.update(id, requestDto);
+        Memo memo = findMemo(id);
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        // memo 내용 수정
+        memo.update(requestDto);
+
+        return id;
     }
 
     public Long deleteMemo(Long id) {
         // 메모 삭제
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
+        Memo memo = findMemo(id);
 
-            memoRepository.delete(id);
+        memoRepository.delete(memo);
 
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        return id;
+    }
+
+    private Memo findMemo(Long id) {
+         return memoRepository.findById(id).orElseThrow(() ->
+                 new IllegalArgumentException("선택한 메모는 존재하지 않습니다.")
+         );
     }
 }
